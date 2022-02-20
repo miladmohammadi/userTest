905108fd13749feca32548482e68867b19adeb81
Milad Mohammadi <mmmiladd@gmail.com>
2022-02-20 23:31:53 +0330

    UI(Users): basic user card

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import UserCard from "./src/users/components/UserCard";
+import {Container, Grid} from "@mui/material";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App" style={{backgroundColor:'#f5f9ea',minHeight:'100vh'}}>
+            <Container maxWidth={'lg'}>
+                <Grid container spacing={2}>
+                    {[1, 2, 3, 4, 5,6,7,8].map((s) => <Grid item xs={3} lg={3} md={4} sm={12}><UserCard/></Grid>)}
+                </Grid>
+            </Container>
+
+        </div>
+    );
 }
 
 export default App;
