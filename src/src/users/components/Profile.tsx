94c8584b679892a064105c3fee03d73ebb56a957
Milad Mohammadi <mmmiladd@gmail.com>
2022-02-28 01:09:18 +0330

    UI(Users): Profile Page

---
+++ src/src/users/components/Profile.tsx
@@ -0,0 +1,156 @@
+import { FunctionComponent, useEffect, useState } from "react";
+import PublicLayout from "../../core/components/PublicLayout";
+import { useParams } from "react-router-dom";
+import * as React from "react";
+import { Avatar, Box, Button, ButtonGroup, Container, Grid, TextField } from "@mui/material";
+import { useCurrentUser } from "../../core/hooks/reduxHooks";
+export interface IProfile {
+  variant: "self" | "other";
+}
+const Profile: FunctionComponent<IProfile> = ({ variant }) => {
+  const [disabled, setDisabled] = useState<boolean>(true);
+  const user = useCurrentUser();
+  let params = useParams();
+
+  return (
+    <PublicLayout maxWidth={"md"}>
+      <Container maxWidth={"md"}>
+        <Grid container spacing={3}>
+          <Grid item xs={12} sx={{ display: "flex", flexDirection: "row-reverse" }}>
+            <ButtonGroup variant="outlined" size="small" disableElevation aria-label="outlined button group">
+              <Button
+                variant={disabled ? "contained" : "outlined"}
+                sx={{ p: 1.5 }}
+                onClick={() => {
+                  setDisabled(true);
+                }}
+              >
+                View
+              </Button>
+              <Button
+                variant={disabled ? "outlined" : "contained"}
+                sx={{ p: 1.5 }}
+                onClick={() => {
+                  setDisabled(false);
+                }}
+              >
+                Edit
+              </Button>
+            </ButtonGroup>
+          </Grid>
+          <Grid item xs={4}>
+            <Avatar
+              variant={"rounded"}
+              sx={{ width: "100%", height: "fit-content", my: 2 }}
+              src={"https://via.placeholder.com/300"}
+            />
+          </Grid>
+          <Grid item xs={8}>
+            <TextField
+              variant={"filled"}
+              fullWidth
+              margin={"normal"}
+              id="email"
+              label={"Email"}
+              name="email"
+              autoComplete="email"
+              autoFocus
+              value={"Milad"}
+              disabled={disabled}
+              // onChange={formik.handleChange}
+              // value={formik.values.email}
+              // error={formik.touched.email}
+              // helperText={formik.touched.email && formik.errors.email}
+            />
+            <TextField
+              variant={"filled"}
+              fullWidth
+              margin={"normal"}
+              id="facebook"
+              label={"Facebook"}
+              name="facebook"
+              autoComplete="facebook"
+              value={"Milad"}
+              disabled={disabled}
+              autoFocus
+              // onChange={formik.handleChange}
+              // value={formik.values.email}
+              // error={formik.touched.email}
+              // helperText={formik.touched.email && formik.errors.email}
+            />
+            <TextField
+              variant={"filled"}
+              fullWidth
+              margin={"normal"}
+              id="twitter"
+              label={"Twitter"}
+              name="twitter"
+              autoComplete="twitter"
+              value={"Milad"}
+              disabled={disabled}
+              autoFocus
+              // onChange={formik.handleChange}
+              // value={formik.values.email}
+              // error={formik.touched.email}
+              // helperText={formik.touched.email && formik.errors.email}
+            />
+            <TextField
+              variant={"filled"}
+              fullWidth
+              margin={"normal"}
+              id="instagram"
+              label={"Instagram"}
+              name="instagram"
+              autoComplete="instagram"
+              value={"Milad"}
+              disabled={disabled}
+              autoFocus
+              // onChange={formik.handleChange}
+              // value={formik.values.email}
+              // error={formik.touched.email}
+              // helperText={formik.touched.email && formik.errors.email}
+            />
+            <TextField
+              variant={"filled"}
+              fullWidth
+              margin={"normal"}
+              id="linkedin"
+              label={"Linkedin"}
+              name="linkedin"
+              autoComplete="linkedin"
+              value={"Milad"}
+              disabled={disabled}
+              autoFocus
+              // onChange={formik.handleChange}
+              // value={formik.values.email}
+              // error={formik.touched.email}
+              // helperText={formik.touched.email && formik.errors.email}
+            />
+            <TextField
+              variant={"filled"}
+              multiline
+              minRows={6}
+              fullWidth
+              margin={"normal"}
+              id="bio"
+              label={"Bio"}
+              name="bio"
+              autoComplete="bio"
+              value={"Milad"}
+              disabled={disabled}
+              autoFocus
+              // onChange={formik.handleChange}
+              // value={formik.values.email}
+              // error={formik.touched.email}
+              // helperText={formik.touched.email && formik.errors.email}
+            />
+            <Box sx={{ display: "flex", width: "100%", flexDirection: "row-reverse", py: 1 }}>
+              <Button variant={"contained"}>Save Changes</Button>
+            </Box>
+          </Grid>
+        </Grid>
+      </Container>
+    </PublicLayout>
+  );
+};
+export default Profile;
--- src/src/users/components/UserList.tsx
+++ src/src/users/components/UserList.tsx
@@ -1,15 +1,45 @@
 import { FunctionComponent } from "react";
+import PublicLayout from "../../core/components/PublicLayout";
+import { Button, Grid, Pagination } from "@mui/material";
+import { Link } from "react-router-dom";
+import UserCard from "./UserCard";
+import * as React from "react";
+import { useGetUsers } from "../../core/hooks/useGetUsers";
 
-const UserList: FunctionComponent<any> = ({ role, page }) => {
-  // const users = useGetUsers()
+const UserList: FunctionComponent = () => {
+  const users = useGetUsers({
+    perPage: 8,
+    initialFilters: null,
+    initialPage: 1,
+  });
+  console.log(users);
   return (
-    <ul>
-      {/*{users?.map((user) => (*/}
-      {/*  <li key={user.id}>*/}
-      {/*    {user.name}, {user.role}*/}
-      {/*  </li>*/}
-      {/*))}*/}
-    </ul>
+    <PublicLayout maxWidth={"lg"}>
+      <Grid item xs={12} sx={{ py: 2, display: "flex", flexDirection: "row-reverse" }}>
+        <Link to={"/user/add"}>
+          <Button>Add new user</Button>
+        </Link>
+      </Grid>
+      <Grid container spacing={2}>
+        {users.list?.map((user) => (
+          <Grid item xs={3} lg={3} md={4} sm={12}>
+            <Link to={`/users/${user.id}`}>
+              <UserCard userData={user} />
+            </Link>
+          </Grid>
+        ))}
+        <Grid item xs={12} sx={{ display: "flex", justifyContent: "center", mt: 6 }}>
+          <Pagination
+            variant="outlined"
+            color="secondary"
+            count={users.totalPage ?? 0}
+            onChange={(e, page) => users.setPage(page)}
+            showFirstButton
+            showLastButton
+          />
+        </Grid>
+      </Grid>
+    </PublicLayout>
   );
 };
 export default UserList;
